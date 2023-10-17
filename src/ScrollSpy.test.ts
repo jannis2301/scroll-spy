fc0d0ed482144ab13f070a7d6fff1e1368f18445
jannis2301 <jannis.ax@gmx.de>
2023-10-17 23:44:58 +0200

    test: added test file

---
+++ src/ScrollSpy.test.ts
@@ -0,0 +1,39 @@
+import { ScrollSpy } from './ScrollSpy'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+
+describe('ScrollSpy', () => {
+  let scrollSpy: ScrollSpy
+
+  beforeEach(() => {
+    const mockElement = document.createElement('div')
+    mockElement.innerHTML = `
+     <header class="header">
+      <nav class="nav">
+        <a class="active nav-link" href="#section-1">Section-1</a>
+        <a class="nav-link" href="#section-2">Section-2</a>
+        <a class="nav-link" href="#section-3">Section-3</a>
+      </nav>
+    </header>
+
+    <section id="section-1">Section 1</section>
+    <section id="section-2">Section 2</section>
+    <section id="section-3">Section 3</section>
+    `
+
+    document.body.appendChild(mockElement)
+    scrollSpy = new ScrollSpy()
+  })
+
+  afterEach(() => {
+    document.body.innerHTML = ''
+  })
+
+  describe('throw', () => {
+    it('should throw an error for an invalid selector', () => {
+      const invalidSelector = '.noElement'
+      expect(() => scrollSpy.throw(invalidSelector)).toThrowError(
+        `Element with selector ${invalidSelector} is missing`
+      )
+    })
+  })
+})
