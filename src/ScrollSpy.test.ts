ea65b95a711cd21420c79dd1d70dfe39a99eccae
jannis2301 <jannis.ax@gmx.de>
2023-10-23 22:22:18 +0200

    test: added test cases for intersectionObserver & hashChange

--- src/ScrollSpy.test.ts
+++ src/ScrollSpy.test.ts
@@ -1,5 +1,15 @@
 import { ScrollSpy } from './ScrollSpy'
 import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import sinon from 'sinon'
+
+const simulateClick = (element: HTMLElement) => {
+  const event = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  })
+  element.dispatchEvent(event)
+}
 
 describe('ScrollSpy', () => {
   let scrollSpy: ScrollSpy
@@ -34,6 +44,80 @@ describe('ScrollSpy', () => {
       expect(() => scrollSpy.throw(invalidSelector)).toThrowError(
         `Element with selector ${invalidSelector} is missing`
       )
+    })
+  })
+
+  it('should return the correct number of navigation links', () => {
+    const navLinks = scrollSpy.getNavLinks()
+    expect(navLinks.length).toEqual(3)
+  })
+
+  it('should return HTMLElements for navigation links', () => {
+    const navLinks = scrollSpy.getNavLinks()
+    navLinks.forEach((link) => {
+      expect(link).toBeInstanceOf(HTMLElement)
+    })
+  })
+
+  it('should return the correct number of sections', () => {
+    const sections = scrollSpy.getSections()
+    expect(sections.length).toEqual(3)
+  })
+
+  it('should return HTMLElements for sections', () => {
+    const sections = scrollSpy.getSections()
+    sections.forEach((section) => {
+      expect(section).toBeInstanceOf(HTMLElement)
+    })
+  })
+
+  it('should update window location hash correctly', async () => {
+    const secondLink = document.querySelectorAll<HTMLElement>('.nav-link')[1]
+    const locationHash = window.location.hash
+    expect(locationHash).toEqual('')
+    simulateClick(secondLink)
+
+    new Promise<void>((resolve) => {
+      setTimeout(() => {
+        expect(locationHash).toEqual('#')
+        resolve()
+      }, 100)
+    })
+  })
+
+  it('should update active navigation link when a section intersects', () => {
+    const secondLink = document.querySelectorAll<HTMLElement>('.nav-link')[1]
+    const secondSection = document.querySelector<HTMLElement>('#section-2')
+    const sections = document.querySelectorAll<HTMLElement>('.section')
+    for (const section of sections) {
+      section.style.height = '200'
+    }
+
+    // Mock Intersection Observer
+    const observeMock = sinon
+      .stub(window, 'IntersectionObserver')
+      .callsFake((callback) => {
+        callback([
+          {
+            isIntersecting: true,
+            target: secondSection,
+          },
+        ])
+        return { observe: () => {} }
+      })
+
+    // Create a custom scroll event
+    const scrollEvent = new WheelEvent('wheel', {
+      deltaY: 300,
+    })
+    window.dispatchEvent(scrollEvent)
+
+    new Promise<void>((resolve) => {
+      setTimeout(() => {
+        expect(secondLink?.classList.contains('active')).toBe(true)
+        observeMock.restore()
+        resolve()
+      }, 100)
     })
   })
 })
